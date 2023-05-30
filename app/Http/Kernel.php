919f10aad2cef8aacd0e7840691d3085e9338d00
wolneycarneiro91 <wolneycarneiro91@gmail.com>
2023-05-29 22:52:56 -0300

    Transactions control using  middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            
         ],
     ];
 
@@ -63,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'transaction' => \App\Http\Middleware\TransactionMiddleware::class,
+
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::apiResource('users', App\Http\Controllers\UserController::class);
+Route::apiResource('users', App\Http\Controllers\UserController::class)->middleware('transaction');
 
 
 Route::prefix('auth')->group(function () {
@@ -24,4 +24,4 @@ Route::prefix('auth')->group(function () {
 Route::get('autenticar',[App\Http\Controllers\RegisterController::class,'verifyPermission'])->middleware('auth:sanctum');
  Route::apiResource('donats',App\Http\Controllers\DonatsController::class);
  
- Route::apiResource('inspections',App\Http\Controllers\InspectionController::class);
+ Route::apiResource('inspections',App\Http\Controllers\InspectionController::class)->middleware('auth:sanctum');
