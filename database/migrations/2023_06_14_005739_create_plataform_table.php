ec8ca89528475d7750277151ca2eee5fbe5cb07f
wolneycarneiro91 <wolneycarneiro91@gmail.com>
2023-06-13 22:22:55 -0300

    Test request

--- database/migrations/2023_06_14_005739_create_plataform_table.php
+++ database/migrations/2023_06_14_005739_create_plataform_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('plataform', function (Blueprint $table) {
+        Schema::create('plataform', function (Blueprint $table) { 
             $table->id();
             $table->string('description');
             $table->timestamps();
